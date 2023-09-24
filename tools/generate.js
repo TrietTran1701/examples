8b00c64254607cec7c470adfdcb5f71384e57f24
Trần Hữu Anh Triết <84529423+TrietTran1701@users.noreply.github.com>
2023-09-24 09:35:22 +0700

    remove unused files

--- tools/generate.js
+++ tools/generate.js
@@ -15,23 +15,4 @@ generateTemplateFiles([
       overwrite: true,
     },
   },
-  {
-    option: 'Create Reduce Action',
-    defaultCase: '(pascalCase)',
-    entry: {
-      folderPath: './tools/templates/react/redux-store/__store__Action.ts',
-    },
-    stringReplacers: ['__store__', '__model__'],
-    dynamicReplacers: [
-      { slot: '__version__', slotValue: config.version },
-      { slot: '__description__', slotValue: config.description },
-    ],
-    output: {
-      path: './src/stores/__store__/__store__(lowerCase)/__store__(pascalCase)Action.ts',
-      pathAndFileNameDefaultCase: '(kebabCase)',
-    },
-    onComplete: (results) => {
-      console.log(`results`, results);
-    },
-  },
 ]);
